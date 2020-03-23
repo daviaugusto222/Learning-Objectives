027da6f26b9e906282741cc7ad701d197c5e96ee
David Augusto <daviaugusto222@gmail.com>
2020-03-23 16:56:43 -0300

    Update Enum.swift

--- Enum.swift
+++ Enum.swift
@@ -1,19 +1,34 @@
-enum Game {
-  case LeagueOfLegends
-  case Terraria
-  case Starbound
-  case Fortnite
+//362	Enum | Enum Case
+//com o CaseIterable os cases do enum podem ser acessado a partir do .allCases, assim facilitando em algumas funcionalidade.
+enum Pokemon: CaseIterable {
+  case bulbasaur, squirtle, chamander
+}
+let numInicial = Pokemon.allCases.count //Contando o total de cases no enum
+print("\(numInicial) pokemons disponíveis, escolha seu inicial:")
+for (index, pokemon) in (Pokemon.allCases).enumerated() {
+  print("\(index + 1) : \(pokemon)")
 }
 
-let jogar = Game.LeagueOfLegends
-switch jogar {
-  case .LeagueOfLegends:
-    print("Você está jogando LoL, sai do ferro!")
-  case .Terraria:
-    print("Você está jogando Terraria, não vai pro hell!")
-  case .Starbound:
-    print("Você está jogando Starbound, cuidado!")
-  case .Fortnite:
-    print("Você está jogando Fortnite, olha a bazuca!")
+//363	Enum | Associated value
+//podemos associar valores adicionais aos enum. Na hora da criação podemos dizer os valores seguindo os tipos passados pelo enum.
+enum Treinador {
+  case pesquisador(String, String, Int) //Nome, Cidade e QuantidadesPokemon
+  case mestre(String, Int) //Nome e QuantidadesPokemon
+  case lider(String, Int, String) //Nome, QuantidadesPokemon e Ginásio
 }
+let drCarvalho = Treinador.pesquisador("Dr. Carvalho", "Kanto", 1040)
+let ash = Treinador.mestre("Ash Catchup", 50)
+let misty = Treinador.lider("Misty", 290, "Água")
+print(drCarvalho, ash, misty)
+
+//364	Enum | Raw value
+//pode ser definido valores padrão para os cases do enum, quando um enum é chamado ele ja inicializa com o valor padrão.
+enum PokemonBusca: Int {
+  case bulbasaur = 1, squirtle = 7, chamander = 4
+}
+if let pokemonNum = PokemonBusca(rawValue:7) {
+  print(pokemonNum)
+}
+
+//365	Enum | Recursive enumerations
 
